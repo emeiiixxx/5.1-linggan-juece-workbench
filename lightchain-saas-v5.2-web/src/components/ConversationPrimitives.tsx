2ccc5d67b6cd2008c8a3992b00e04922fcd854ae
emeiiixxx <emeiiixxx@gmail.com>
2026-08-17 14:29:54 +0800

    feat: show reference count

--- lightchain-saas-v5.2-web/src/components/ConversationPrimitives.tsx
+++ lightchain-saas-v5.2-web/src/components/ConversationPrimitives.tsx
@@ -837,7 +837,7 @@ export function TaskDetailPanel({
                     type="button"
                     onClick={() => setReferenceExpanded(true)}
                   >
-                    <span>{t("查看全部")}</span>
+                    <span>{t("查看全部")} {references.length} 条</span>
                     <FigmaIcon name="chevron-right" size={16} />
                   </button>
                 ) : null}
